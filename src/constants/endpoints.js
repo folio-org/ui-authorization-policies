3bd35fd154d41dc3400ed83bb5dc495e9cf10e35
Zak Burke <zburke@ebsco.com>
2024-03-13 16:21:53 -0400

    server-side query sort (#14)
    This isn't a final solution since policies may need to be i18n'ed, but
    at least it makes the UI more usable while we work on that feature.

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -2,9 +2,9 @@ import { likeSearch, allRecords } from './queries';
 
 export const POLICIES_ENDPOINT = (searchTerm, limit = 1000) => {
   if (!searchTerm) {
-    return `policies?limit=${limit}&query=${allRecords}`;
+    return `policies?limit=${limit}&query=${allRecords} sortby name`;
   }
   return `policies?limit=${limit}&query=name=${likeSearch(
     encodeURIComponent(searchTerm)
-  )}`;
+  )} sortby name`;
 };
--- src/constants/endpoints.test.js
+++ src/constants/endpoints.test.js
@@ -2,9 +2,9 @@ import { POLICIES_ENDPOINT } from './endpoints';
 
 describe('Endpoints', function () {
   it('test url for policies', () => {
-    expect(POLICIES_ENDPOINT()).toBe('policies?limit=1000&query=cql.allRecords=1');
+    expect(POLICIES_ENDPOINT()).toBe('policies?limit=1000&query=cql.allRecords=1 sortby name');
     expect(POLICIES_ENDPOINT('searchTest')).toBe(
-      'policies?limit=1000&query=name=*searchTest*'
+      'policies?limit=1000&query=name=*searchTest* sortby name'
     );
   });
 });
